29741f0442010422a406d7c38c1736b4019be564
Darknab <117382045+Darknab@users.noreply.github.com>
2023-12-16 09:12:43 +0100

    Complete fiest draft of the Projects section

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,20 @@
 const startAnimation = (entries, observer) => {
-    entries.forEach(entry => {
-      entry.target.classList.toggle("scroll-up", entry.isIntersecting);
-    });
-  };
+  entries.forEach(entry => {
+    //if (!entry.target.classList.contains("animate")) {
+      entry.target.classList.toggle("animate", entry.isIntersecting);
+    //}
+  });
+};
   
-  const observer = new IntersectionObserver(startAnimation);
-  const options = { root: null, rootMargin: '0px', threshold: 1 }; 
+const observer = new IntersectionObserver(startAnimation);
+const options = { root: null, rootMargin: '0px', threshold: 1 }; 
   
-  const elements = document.querySelectorAll('#skills .container');
+const elements = document.querySelectorAll('#skills .container');
   elements.forEach(el => {
     observer.observe(el, options);
   });
+
+/*const cards = document.querySelectorAll('#projects .container');
+cards.forEach(el => {
+  observer.observe(el, options);
+})*/
