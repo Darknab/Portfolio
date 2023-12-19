318f9cf63dae0a04754161056325387c9d02031f
Darknab <117382045+Darknab@users.noreply.github.com>
2023-12-19 10:52:06 +0100

    Fix animations

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,20 +1,20 @@
 const startAnimation = (entries, observer) => {
   entries.forEach(entry => {
-    //if (!entry.target.classList.contains("animate")) {
+    if (!entry.target.classList.contains("animate")) {
       entry.target.classList.toggle("animate", entry.isIntersecting);
-    //}
+    }
   });
 };
   
 const observer = new IntersectionObserver(startAnimation);
 const options = { root: null, rootMargin: '0px', threshold: 1 }; 
   
-const elements = document.querySelectorAll('#skills .container');
+const elements = document.querySelectorAll('#skills li');
   elements.forEach(el => {
     observer.observe(el, options);
   });
 
-/*const cards = document.querySelectorAll('#projects .container');
+const cards = document.querySelectorAll('#projects .card');
 cards.forEach(el => {
   observer.observe(el, options);
-})*/
+})
